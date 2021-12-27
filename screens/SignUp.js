6c0b14b90d96e1bd11656eb40f74b60946bb8faa
Calvin Putera Loka <calvinputeraloka@gmail.com>
2021-12-27 23:52:23 +0700

    add packages and create a SignUp screen

---
+++ screens/SignUp.js
@@ -0,0 +1,311 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Modal,
+  FlatList,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from "react-native";
+import { COLORS, SIZES, FONTS, icons, images } from "../constants";
+import { LinearGradient } from "expo-linear-gradient";
+
+const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [areas, setAreas] = useState([]);
+  const [selectedArea, setSelectedArea] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        let areaData = data.map((item) => {
+          return {
+            code: item.cca2,
+            name: item.name.common,
+            callingCode: `+${item.idd.suffixes}`,
+            flags: `https://countryflagsapi.com/png/${item.cca2}`,
+          };
+        });
+
+        setAreas(areaData);
+
+        if (areaData.length > 0) {
+          let defaultData = areaData.filter((a) => a.code == "DJ");
+
+          if (defaultData.length > 0) {
+            setSelectedArea(defaultData[0]);
+          }
+        }
+      });
+  }, []);
+
+  function renderHeader() {
+    return (
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2,
+        }}
+        onPress={() => console.log("Sign Up")}
+      >
+        <Image
+          source={icons.back}
+          resizeMode="contain"
+          style={{ width: 20, height: 20, tintColor: COLORS.white }}
+        />
+        <Text
+          style={{
+            marginLeft: SIZES.padding * 1.5,
+            color: COLORS.white,
+            ...FONTS.h4,
+            fontFamily: "Roboto",
+            fontWeight: "normal",
+            fontSize: 18,
+            lineHeight: 22,
+          }}
+        >
+          Sign Up
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
+  function renderLogo() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 5,
+          height: 100,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={images.wallieLogo}
+          resizeMode="contain"
+          style={{ width: "60%" }}
+        />
+      </View>
+    );
+  }
+
+  function renderForm() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 3,
+          marginHorizontal: SIZES.padding * 3,
+        }}
+      >
+        {/* Full Name */}
+        <View style={{ marginTop: SIZES.padding * 3 }}>
+          <Text
+            style={{
+              color: COLORS.lightGreen,
+              fontFamily: "Roboto",
+              fontSize: 16,
+              lineHeight: 22,
+            }}
+          >
+            Full Name
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              fontFamily: "Roboto",
+              fontSize: 16,
+              lineHeight: 22,
+            }}
+            placeholder="Enter Full Name"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+          />
+        </View>
+
+        {/* Phone Number */}
+        <View style={{ marginTop: SIZES.padding * 2 }}>
+          <Text
+            style={{
+              color: COLORS.lightGreen,
+              fontFamily: "Roboto",
+              fontSize: 16,
+              lineHeight: 22,
+            }}
+          >
+            Number
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            {/* Country Code */}
+            <TouchableOpacity
+              style={{
+                width: 100,
+                height: 50,
+                marginHorizontal: 5,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                flexDirection: "row",
+                fontFamily: "Roboto",
+                fontSize: 20,
+                lineHeight: 30,
+              }}
+              onPress={() => console.log("Modal")}
+            >
+              <View style={{ justifyContent: "center" }}>
+                <Image
+                  source={icons.down}
+                  style={{ width: 10, height: 10, tintColor: COLORS.white }}
+                />
+              </View>
+              <View style={{ justifyContent: "center", marginLeft: 5 }}>
+                <Image
+                  source={{ uri: selectedArea.flags }}
+                  resizeMode="contain"
+                  style={{ width: 30, height: 30 }}
+                />
+              </View>
+              <View style={{ justifyContent: "center", marginLeft: 5 }}>
+                <Text
+                  style={{
+                    color: COLORS.white,
+                    fontFamily: "Roboto",
+                    fontSize: 16,
+                    lineHeight: 22,
+                  }}
+                >
+                  {selectedArea.callingCode}
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TextInput
+              style={{
+                flex: 1,
+                paddingBottom: 12,
+                marginVertical: SIZES.padding,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                height: 40,
+                color: COLORS.white,
+                fontFamily: "Roboto",
+                fontSize: 16,
+                lineHeight: 30,
+              }}
+              maxLength={12}
+              placeholder="Enter Phone Number"
+              placeholderTextColor={COLORS.white}
+              selectionColor={COLORS.white}
+              keyboardType="numeric"
+            />
+          </View>
+        </View>
+
+        {/* Password */}
+        <View style={{ marginTop: SIZES.padding * 2 }}>
+          <Text
+            style={{
+              color: COLORS.lightGreen,
+              fontFamily: "Roboto",
+              fontSize: 16,
+              lineHeight: 22,
+            }}
+          >
+            Password
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              fontFamily: "Roboto",
+              fontSize: 16,
+              lineHeight: 22,
+            }}
+            placeholder="Enter Password"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+            secureTextEntry={!showPassword}
+          />
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              right: 5,
+              bottom: 10,
+              height: 30,
+              width: 30,
+            }}
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <Image
+              source={showPassword ? icons.disable_eye : icons.eye}
+              style={{ height: 20, width: 20, tintColor: COLORS.white }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function renderButton() {
+    return (
+      <View style={{ margin: SIZES.padding * 3 }}>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            backgroundColor: COLORS.black,
+            borderRadius: SIZES.radius / 1.5,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => console.log("to Home")}
+        >
+          <Text
+            style={{
+              color: COLORS.white,
+              fontFamily: "Roboto",
+              fontSize: 16,
+              lineHeight: 22,
+            }}
+          >
+            Continue
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "android" ? null : "height"}
+      style={{ flex: 1 }}
+    >
+      <LinearGradient
+        colors={[COLORS.lime, COLORS.emerald]}
+        style={{ flex: 1 }}
+      >
+        <ScrollView>
+          {renderHeader()}
+          {renderLogo()}
+          {renderForm()}
+          {renderButton()}
+        </ScrollView>
+      </LinearGradient>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default SignUp;
